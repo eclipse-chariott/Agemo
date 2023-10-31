cfce1f3c9098d1c8d0e5ef8500d8a241f428f4d6
Devin Kelley <105753233+devkelley@users.noreply.github.com>
2023-10-31 14:49:59 -0700

    Add comment to bash script

--- container_startup.sh
+++ container_startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+# SPDX-License-Identifier: MIT
+
+# Copy any configuration files present to service configuration.
 cp -rn /mnt/config /sdv/.agemo
 
 /sdv/service
