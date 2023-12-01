a4def3924a7a4d57ea7477a8c0138f39e157b887
Devin Kelley <105753233+devkelley@users.noreply.github.com>
2023-12-01 15:44:11 -0800

    reword

--- container/scripts/container_startup.sh
+++ container/scripts/container_startup.sh
@@ -9,7 +9,7 @@ set -eu
 
 # Copy any mounted configuration files present to service configuration at runtime.
 # If there is a configuration file with the same name at `/sdv/.agemo/config` this will overwrite
-# the file with the mounted configuration file. 
+# that file with the mounted configuration file. 
 cp -rf /mnt/config /sdv/.agemo
 
 /sdv/service
